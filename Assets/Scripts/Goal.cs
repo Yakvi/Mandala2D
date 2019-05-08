ae4f17aaa324504322dfc1b1ab5a642bb05f2767
Ivan <ivankoy@gmail.com>
2019-05-08 18:02:21 +0200

    Started work on UI

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -6,10 +6,39 @@ using UnityEngine;
 public class Goal : MonoBehaviour
 {
     public TextMeshPro ScoreText;
-    public int score = 0;
+    public GameObject Active;
+    public GameObject Disabled;
+    public int Score;
+    public bool IsActive;
 
-    private void OnCollisionEnter2D(Collision2D other) {
-        ScoreText.text = score++.ToString();
-        GameObject.Destroy(other.gameObject);
+    private void Awake()
+    {
+        ScoreText.gameObject.SetActive(true);
+        Active.SetActive(true);
+        Disabled.SetActive(false);
+        Score = 0;
+        IsActive = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (IsActive)
+        {
+            ++Score;
+            ScoreText.text = Score.ToString();
+            GameObject.Destroy(other.gameObject);
+        }
+    }
+
+    public void Complete()
+    {
+        if (IsActive)
+        {
+            GetComponent<AudioSource>().Play();
+            ScoreText.gameObject.SetActive(false);
+            Active.SetActive(false);
+            Disabled.SetActive(true);
+            IsActive = false;
+        }
     }
 }
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -8,56 +8,54 @@ public class LevelManager : MonoBehaviour
     public List<Goal> Goals;
     public int MaxScore;
     public int GoalsCompleted;
-    public Color LevelColor;
     public Surface DefaultSurface;
+    public Animator UIAnimator;
 
-    Surface newSurface;
+    bool LevelComplete = false;
 
     private void Update()
     {
         // Update new surface 
-        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePos.z = 0;
-        // if (newSurface)
-        // {
-        //     var width = newSurface.transform.position - mousePos;
-        //     newSurface.Sprite.size = new Vector2(width.magnitude, 0.05f);
-        //     newSurface.Col.size = new Vector2(width.magnitude, 0.05f);
-        //     newSurface.transform.rotation = Quaternion.LookRotation(gameObject.transform.position - mousePos, Vector3.forward);
-
-        //     if (Input.GetButtonUp("Fire1"))
-        //     {
-        //         newSurface = null;
-        //     }
-        // }
-        // else 
         if (Input.GetButton("Fire1"))
         {
-            newSurface = GameObject.Instantiate(DefaultSurface, mousePos, Quaternion.identity);
+            var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+            GameObject.Instantiate(DefaultSurface, mousePos, Quaternion.identity);
         }
 
         // Check goal completion
         foreach (var goal in Goals)
         {
-            if (goal.score >= MaxScore)
+            if (goal.Score >= MaxScore && goal.IsActive)
             {
+                goal.Complete();
                 ++GoalsCompleted;
             }
         }
 
         // Level complete
-        if (GoalsCompleted == Goals.Count)
+        if (GoalsCompleted == Goals.Count && !LevelComplete)
         {
-            var currentScene = SceneManager.GetActiveScene().buildIndex;
-            var sceneCount = SceneManager.sceneCountInBuildSettings;
-            if (currentScene + 1 < sceneCount)
+            LevelComplete = true;
+            if (UIAnimator)
             {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                var isComplete = UIAnimator.GetBool("Level Complete");
+                UIAnimator.SetBool("Level Complete", !isComplete);
             }
-            else
-            {
-                SceneManager.LoadScene(0);
-            }
+        }
+    }
+
+    public void NextLevel()
+    {
+        var currentScene = SceneManager.GetActiveScene().buildIndex;
+        var sceneCount = SceneManager.sceneCountInBuildSettings;
+        if (currentScene + 1 < sceneCount)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
         }
     }
 }
